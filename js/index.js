73394f36cd9c42521c4a0dd83396ca45eb06d0a1
margarita2104 <margarita.p.ferreira@gmail.com>
2023-11-14 20:55:42 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -30,7 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
 // swiper
 
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
   effect: "fade",
@@ -39,12 +38,10 @@ const swiper = new Swiper(".swiper", {
     delay: 5000,
   },
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
